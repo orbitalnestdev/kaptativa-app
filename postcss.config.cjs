d760e956cd011c068f203c188d22057c2d23eda9
Eliana Azcurra <azcurraely@gmail.com>
2026-07-25 18:21:48 -0300

    perf: aggressively deduplicate and purge bloated CSS, fix syntax errors, remove unused stylesheets to optimize FCP and LCP

---
+++ postcss.config.cjs
@@ -0,0 +1,1 @@
+module.exports = { plugins: [ require('@fullhuman/postcss-purgecss')({ content: ['./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}'], safelist: [/active/, /open/, /light-bg/, /dark-bg/, /success/, /error/, /active-tab/, /dragover/, /active-template-card/, /active-block-card/, /reunion-comercial/, /soporte-tecnico/, /today/, /scrolled/, /header-scrolled/, /reveal/] }) ] };
